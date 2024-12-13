e8097e185dbfed258692af8415bcd165a260ca9e
whoisedum <eduardoolmedo224@gmail.com>
2024-12-13 10:23:12 -0500

    primer commit

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nombre')
+                ->required()
+                ->maxLength(50),
+                Forms\Components\Select::make('categoria')
+                    ->options([
+                        'lacteos' => 'Lacteos',
+                        'enlatadas' => 'Enlatadas',
+                        'embutidos' => 'Embutidos',
+                        'limpieza' => 'Limpieza',
+                        'refrescos' => 'Refrescos',
+                        'dulces' => 'Dulces',
+                    ])
+                    ->required(),
+                Forms\Components\TextInput::make('cantidad')
+                ->required()
+                ->maxLength(50),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('categoria')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('cantidad')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
